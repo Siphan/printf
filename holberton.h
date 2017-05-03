a9a9462dee8f048a700a788155d01bb2a4d23b1f
Siphan Bou <siphan.bou@gmail.com>
2017-05-03 02:27:04 +0200

    Add comments to header prototypes

--- holberton.h
+++ holberton.h
@@ -1,7 +1,18 @@
 #include <stdarg.h>
+/*  * _printf - print to standard output one char at a time */
 int _printf(const char *format, ...);
+
+/* conversion - check the conversion specifiers */
 int conversion(va_list args, char specifier);
+
+/* ccase - handle the character conversion specifier */
 int ccase(va_list args);
+
+/* putchr - print a char to stdout */
 int putchr(char c);
+
+/* s_case - handle the string conversion specifier */
 int s_case(va_list args);
+
+/* putstr - print a string to stdout */
 int putstr(char *str);
